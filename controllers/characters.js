d43c89f168c8e9648a5684397d58969fef9ae4a6
nerasan <salima.harun@gmail.com>
2020-11-10 16:39:42 -0600

    added axios and character index/show pages

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,7 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const axios = require('axios')
 const passport = require('../config/ppConfig.js')
+
+// GET /characters -- lists all characters from axios get
+
+
+router.get('/', (req, res)=>{
+    axios.get(`https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters?name=`)
+    .then(response=>{
+        // console.log(response.data)
+        res.render('characters/index', {characterData: response.data})
+    })
+    .catch(err=>{
+        console.log("the error is:", err)
+    })
+})
+
+// GET /characters/:id -- lists information about a specific character
+router.get('/:id', (req, res)=>{
+    let character = req.params.id
+    axios.get(`https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters/${character}`)
+    .then(response=>{
+        console.log(response.data)
+        res.render('characters/show', {characterData: response.data})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 
 
 
--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use((req, res, next)=>{
 
 // use controllers
 app.use('/auth', require('./controllers/auth.js'))
+app.use('/characters', require('./controllers/characters.js'))
 
 app.get('/', (req, res)=>{
     res.render('home')
